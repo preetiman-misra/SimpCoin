d366645ed3d3e71cde4695ace6b90177289a449e
Bitthal <pmisra50@gmail.com>
2020-11-10 13:16:16 +0530

    Refactored code, added TRANSACTION SIGNING and incorporated dotenv.

---
+++ src/main.js
@@ -0,0 +1,21 @@
+const { BlockChain, Transaction } = require("./blockchain");
+const EC = require("elliptic").ec;
+const ec = new EC("secp256k1");
+require("dotenv").config();
+
+const myKey = ec.keyFromPrivate(process.env.PRIVATE_KEY);
+const myWalletAddress = ec.getPublicKey("hex");
+
+let simpCoin = new BlockChain();
+
+const tx1 = new Transaction(myWalletAddress, "public key goes here", 10);
+tx1.signTransaction(myKey);
+simpCoin.addTransaction(tx1);
+
+console.log("\n Starting the miner....");
+simpCoin.minePendingTransactions("preetimans-address");
+
+console.log(
+  "\n Balance of Preetiman is ",
+  simpCoin.getBalanceOfAddress("preetimans-address")
+);
